94ddff59923b660044d8de853507cde119b8fc6b
Nurs <nursultan_erzhan@mail.ru>
2022-04-04 22:54:14 +0600

    Lesson_02

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const productName = document.getElementById('name');
+const productAddModal = document.getElementsByClassName('modal__form');
+const productHasDiscount = document.getElementById('discount');
+const productDiscountCount = document.getElementById('discount_count');
