1f78c29c5898ad58a883090eae0e18fd92d82de1
Nurs <nursultan_erzhan@mail.ru>
2022-04-19 23:42:04 +0600

    lesson07

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 let goods = [
     {
         "id": 1,
-        "title": "Смартфон Xiaomi 11T 8/128GB",
+        "name": "Смартфон Xiaomi 11T 8/128GB",
         "price": 27000,
         "description": "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
         "category": "mobile-phone",
@@ -17,7 +17,7 @@ let goods = [
     },
     {
         "id": 2,
-        "title": "Радиоуправляемый автомобиль Cheetan",
+        "name": "Радиоуправляемый автомобиль Cheetan",
         "price": 4000,
         "description": "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
         "category": "toys",
@@ -31,7 +31,7 @@ let goods = [
     },
     {
         "id": 3,
-        "title": "ТВ приставка MECOOL KI",
+        "name": "ТВ приставка MECOOL KI",
         "price": 12400,
         "description": "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
         "category": "tv-box",
@@ -45,7 +45,7 @@ let goods = [
     },
     {
         "id": 4,
-        "title": "Витая пара PROConnect 01-0043-3-25",
+        "name": "Витая пара PROConnect 01-0043-3-25",
         "price": 22,
         "description": "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
         "category": "cables",
@@ -64,7 +64,7 @@ const createRow = goodObject => {
   <tr>
     <td class="table__cell ">${goodObject.id}</td>
     <td class="table__cell table__cell_left table__cell_name" data-id="${goodObject.id}">
-        <span class="table__cell-id">id: ${goodObject.id}</span>${goodObject.title}</td>
+        <span class="table__cell-id">id: ${goodObject.id}</span>${goodObject.name}</td>
     <td class="table__cell table__cell_left">${goodObject.category}</td>
     <td class="table__cell">${goodObject.units}</td>
     <td class="table__cell">${goodObject.count}</td>
@@ -97,12 +97,19 @@ const addButton = document.querySelector('.panel__add-goods');
 addButton.addEventListener('click', () => {
     const overlay = document.querySelector('.overlay');
     overlay.classList.add('active');
+    const vendorCodeId = Math.round(Math.random() * (10 ** 14));
+    const vendorCodeSpan = document.querySelector('.vendor-code__id');
+    vendorCodeSpan.textContent = vendorCodeId;
 });
+
+const closeModal = () => {
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.remove('active');
+};
 
 const closeButton = document.querySelector('.modal__close');
 closeButton.addEventListener('click', () => {
-    const overlay = document.querySelector('.overlay');
-    overlay.classList.remove('active');
+    closeModal();
 });
 
 const tbody = document.querySelector('tbody');
@@ -114,8 +121,56 @@ tbody.addEventListener('click', e => {
         const id = parentTr.firstElementChild.textContent;
         goods = goods.filter(item => item.id != id);
         parentTr.remove();
+        computeTotalPriceOfGoods();
     }
 });
 
+const modalForm = document.querySelector('.modal__form');
+modalForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const product = Object.fromEntries(formData);
+    product.id = document.querySelector('.vendor-code__id').textContent;
+    const newRow = createRow(product);
+    addToBody(newRow);
+    product.discont = product.discount_count;
+    goods.push(product);
+    console.log(product);
+    computeTotalPriceOfGoods();
+    closeModal();
+});
+
+modalForm.discount.addEventListener('click', () => {
+    modalForm.discount_count.toggleAttribute("disabled");
+    modalForm.discount_count.value = '';
+});
+
+const computeCurrentModalTotalPrice = () => {
+
+    if (!Number.isNaN(modalForm.count.value) && !Number.isNaN(modalForm.price.value)) {
+
+        let totalPrice = modalForm.count.value * modalForm.price.value;
+        if (!Number.isNaN(modalForm.discount_count.value))
+            totalPrice -= modalForm.discount_count.value;
+
+        modalForm.total.textContent = totalPrice;
+    }
+};
+
+modalForm.count.addEventListener('change', computeCurrentModalTotalPrice);
+modalForm.price.addEventListener('change', computeCurrentModalTotalPrice);
+modalForm.discount_count.addEventListener('change', computeCurrentModalTotalPrice);
+
+
+const computeTotalPriceOfGoods = () => {
+    let totalPriceOfGoods = goods.reduce((total, item) => {
+        if (!Number.isNaN(item.discont))
+            return total + (item.price * item.count - item.discont);
+        else total + (item.price * item.count);
+    }, 0);
+    const crmTotalPriceSpan = document.querySelector('.crm__total-price');
+    crmTotalPriceSpan.textContent = `$ ${totalPriceOfGoods}`;
+};
 
 renderGoods(goods);
+computeTotalPriceOfGoods();
